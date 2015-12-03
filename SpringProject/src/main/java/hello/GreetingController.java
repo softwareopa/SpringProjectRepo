b0a368a553c8f1b64ebd3cfe467997ed61a0a914
Ramon Schalleck <ramon_schalleck@hotmail.com>
2015-12-03 15:44:15 +0100

    Notice Homework: INtegration with Chorus

--- SpringProject/src/main/java/hello/GreetingController.java
+++ SpringProject/src/main/java/hello/GreetingController.java
@@ -11,9 +11,21 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @RequestMapping(value="/greeting", produces = "application/json; charset=utf-8")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
     }
+    
+    
+  /**
+   *    TODO: expand songs with more attributes
+   *    
+   *    /songs --> return full list of songs known by the system
+   *    /songs/{title} --> return song if songs exists, otherwise HTTP 404
+   *    /singers/ --> full list of singers
+   *    /singers/{name} --> return singer if exists, otherwise 404
+   *    /directors/
+   *    /directors/{name}
+   */
 }
